caa034cb90bd03bada5630553166eff3a17800ca
rubensmk <rubenskishimoto@gmail.com>
2022-11-24 23:58:16 -0300

    Add new article page, index article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,24 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end
