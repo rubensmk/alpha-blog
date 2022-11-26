1e24748d5beb89de9840c383a48497da9e258cec
rubensmk <rubenskishimoto@gmail.com>
2022-11-26 15:59:01 -0300

    changes on styling and fix form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
 
+  def show; end
+
   def index
     @articles = Article.all
   end
-
-  def show; end
 
   def new
     @article = Article.new
